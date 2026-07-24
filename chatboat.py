27e68f4c743a9823f6cf6ceb35e32a16f14b695c
shamzee <zsham433@gmail.com>
2026-07-25 04:28:43 +0500

    Check cloud package versions

--- chatboat.py
+++ chatboat.py
@@ -48,7 +48,7 @@ if user_input:
 
             outputs = model.generate(
                 **inputs,
-                max_new_tokens=50,
+                max_new_tokens=25,
                 do_sample=False,
                 pad_token_id=tokenizer.eos_token_id,
                 eos_token_id=tokenizer.eos_token_id,
